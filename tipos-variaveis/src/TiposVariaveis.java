770a608547eb97758b1d63796876146895010e4c
Walter <wljrodrigues@gmail.com>
2022-11-20 23:43:57 -0300

    Atualização.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //estudar tipos primitivos
+        //estudara classe String que representa texto na aplicação.
+        
+        String meuNome = "Walter Rodrigues";
+        
+        double salarioMinimo = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+		System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;		
+		
     }
 }
